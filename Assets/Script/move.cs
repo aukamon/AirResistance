991eb790e00b96a0938d80680454ac18a6893500
Thanasit Robsuntisuk <thanasit.robs@bumail.net>
2024-03-15 12:26:21 +0700

    finished

--- Assets/Script/move.cs
+++ Assets/Script/move.cs
@@ -10,21 +10,18 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.Space))
         {
             rb.AddForce(transform.forward * enginePowerThrust);
         }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(transform.forward * enginePowerThrust*-1);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(transform.right * enginePowerThrust*-1);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(transform.right * enginePowerThrust);
-        }
+
+        Vector3 lift = Vector3.Project(rb.velocity, transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+
+        rb.drag = rb.velocity.magnitude * angularDrag;
+
+        rb.AddTorque(Input.GetAxis("Horizontal") * transform.forward * -1);
+
+        rb.AddTorque(Input.GetAxis("Vertical") * transform.right);
     }
 }
